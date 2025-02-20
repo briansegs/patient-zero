04d202a8b177dc8897075734080da8139c69f4d5
bsegers <briansegs@gmail.com>
2025-02-19 20:57:10 -0500

    Image loader fix attempt 2

--- next.config.js
+++ next.config.js
@@ -6,11 +6,13 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
+const VERCEL_URL = process.env.VERCEL_URL
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL, 'https://*.vercel.app'].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL, VERCEL_URL].map((item) => {
         const url = new URL(item)
 
         return {
