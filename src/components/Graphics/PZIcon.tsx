451885232f959297b14b250332005367a368883b
bsegers <briansegs@gmail.com>
2025-02-23 17:01:20 -0500

    Add missing height and width to PZIcon

--- src/components/Graphics/PZIcon.tsx
+++ src/components/Graphics/PZIcon.tsx
@@ -8,6 +8,8 @@ export const Icon = () => {
       alt="Patient Zero Icon"
       loading={'eager'}
       fetchPriority={'high'}
+      height={32}
+      width={32}
       decoding="async"
       className={clsx('size-full rounded-lg dark:bg-neutral-100 dark:pb-[.5px]')}
       src="/Patient-Zero_Icon.svg"
