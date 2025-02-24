e1031db48ea7f95900f7efddba44e5b3d08671ee
bsegers <briansegs@gmail.com>
2025-02-23 19:20:30 -0500

    Clean up Sign in / Sign up component | Improve accessibility

--- src/components/SignInSignSignUp/SignInSignSignUp.tsx
+++ src/components/SignInSignSignUp/SignInSignSignUp.tsx
@@ -5,13 +5,18 @@ const SignInSignSignUp = () => {
   return (
     <div className="relative -mt-[10.4rem] flex h-[60vh] items-center justify-center bg-white">
       <div className="container mt-28 flex h-full flex-col items-center justify-center gap-6">
-        <p className="text-2xl font-bold">Sign in or sign up to gain access.</p>
+        <h1 className="text-2xl font-bold">Sign in or sign up to gain access.</h1>
         <div className="flex gap-4">
-          <Button className={''} size={'lg'} variant={'default'}>
+          <Button size={'lg'} variant={'default'} aria-label="Sign in to your account">
             <SignInButton />
           </Button>
 
-          <Button className={'text-black'} size={'lg'} variant={'outline'}>
+          <Button
+            className={'text-black'}
+            size={'lg'}
+            variant={'outline'}
+            aria-label="Create a new account"
+          >
             <SignUpButton />
           </Button>
         </div>
