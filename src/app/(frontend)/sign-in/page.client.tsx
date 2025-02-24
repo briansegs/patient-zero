a55a292e353e0fd53b82bb193e1addd5e628b729
bsegers <briansegs@gmail.com>
2025-02-24 10:53:44 -0500

    Address build errorattempt 1

--- src/app/(frontend)/sign-in/page.client.tsx
+++ src/app/(frontend)/sign-in/page.client.tsx
@@ -1,14 +1,21 @@
 'use client'
 import { useHeaderTheme } from '@/providers/HeaderTheme'
+import { useAuth } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 const PageClient: React.FC = () => {
-  /* Force the header to be dark mode while we have an image behind it */
   const { setHeaderTheme } = useHeaderTheme()
+  const { isSignedIn } = useAuth()
+  const router = useRouter()
 
   useEffect(() => {
     setHeaderTheme('light')
-  }, [setHeaderTheme])
+
+    if (isSignedIn) {
+      router.push('/')
+    }
+  }, [isSignedIn, setHeaderTheme, router])
   return <React.Fragment />
 }
 
