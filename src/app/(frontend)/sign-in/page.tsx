610403d246e01adcda149f404b280361d05068e8
bsegers <briansegs@gmail.com>
2025-02-23 22:24:55 -0500

    Add dark theme styles to sign in page

--- src/app/(frontend)/sign-in/page.tsx
+++ src/app/(frontend)/sign-in/page.tsx
@@ -15,7 +15,7 @@ const SignIn = async () => {
   return (
     <div className="pb-24 pt-16">
       <PageClient />
-      <div className="relative -mt-[10.4rem] flex h-[60vh] items-center justify-center bg-white">
+      <div className="relative -mt-[10.4rem] flex h-[60vh] items-center justify-center bg-white dark:bg-black">
         <div className="container mt-28 flex h-full flex-col items-center justify-center gap-6">
           <h1 className="text-2xl font-bold">Sign in or sign up to gain access.</h1>
           <div className="flex gap-4">
@@ -24,7 +24,7 @@ const SignIn = async () => {
             </Button>
 
             <Button
-              className={'text-black'}
+              className={'text-black dark:text-white'}
               size={'lg'}
               variant={'outline'}
               aria-label="Create a new account"
